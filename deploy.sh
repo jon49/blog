6cb90f79cafd031d7553adc57ceafb4dc021fe0a
Jon Nyman <nymanjon@gmail.com>
2016-12-04 12:42:42 -0700

    Updated deploy file to make hugo before committing.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
+make cleanAll && make posts && hugo
 echo "Please enter commit message."
 read commit
-make cleanAll && make posts && hugo
 git add -A && git commit -m "$commit"
 git push origin master
 git subtree push --prefix=public https://github.com/jon49/blog.git gh-pages
