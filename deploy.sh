c3a51ff140ec895a72b36537349ee855d6395620
Jon Nyman <nymanjon@gmail.com>
2017-02-12 14:03:10 -0700

    Added post the power of static typing

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ make cleanAll && make static && make posts && hugo
 echo "Please enter commit message."
 read commit
 git add -A && git commit -m "$commit"
-git push origin +master
-git subtree push --prefix=public https://github.com/jon49/blog.git gh-pages
+git push origin master --force
+git push origin `git subtree split --prefix=public master`:gh-pages --force
+# git subtree push --prefix=public https://github.com/jon49/blog.git gh-pages
